c8dc3a49caec41a453f493a809605d85ee9f276a
Camilo Hurtado <chc201627@gmail.com>
2020-02-11 19:50:51 -0500

    add file LinkFormContainer

---
+++ mvp-devf-backend/src/containers/LinkFormContainer/LinkFormContainer.js
@@ -0,0 +1,6 @@
+import React, {useState} from 'react';
+import { useParams } from 'react-router-dom';
+import FadeIn from 'react-fade-in';
+import { authForm } from '../../components/forms/authForm';
+
+
