7f09bf06978ea5ec0ad9feed123811c197bf73b5
Camilo Hurtado <chc201627@gmail.com>
2020-02-12 11:27:45 -0500

    add button form

--- mvp-devf-backend/src/components/forms/LinkForm.js
+++ mvp-devf-backend/src/components/forms/LinkForm.js
@@ -4,49 +4,55 @@ import React from 'react';
 export const LinkForm = () => {
 
     return (
-
-            
+        <form>
+             <div className="field">
+                    <label for="" className="label">Título</label>
+                    <div className="control has-icons-left">
+                        <input type="text" placeholder="titulo" className="input is-rounded" required/>
+                        <span className="icon is-small is-left">
+                            <i class="fas fa-heading"></i>
+                        </span>
+                    </div>
+                </div>
             <div className="field">
-                <label className="label">
-                    Titulo
-                </label>
-                <div className="control">
-                    <input className="input is-info" type="text" placeholder="Titulo"/>
-                </div>
-                <label className="label">
-                   Descripción
-                </label>
-                <div className="control">
-                    <input className="input is-info" type="text" placeholder="Descripcion"/>
-                </div>
-                <label className="label">
-                    URL
-                </label>
-                <div className="control">
-                    <input className="input is-info" type="text" placeholder="URL"/>
-                </div>
-                <label class="label">Subject</label>
-                    <div class="control">
-                         <div class="select">
-                             <select>
-                                <option>Select dropdown</option>
-                                <option>With options</option>
-                         </select>
-                 </div>
+                    <label for="" className="label">Descripción</label>
+                    <div className="control has-icons-left">
+                        <input type="text" placeholder="descripción" className="input is-rounded" required/>
+                        <span className="icon is-small is-left">
+                        <i class="fas fa-audio-description"></i>    
+                        </span>
+                    </div>
             </div>
-            <div className="field is-grouped is-centered">
-  <p className="control">
-    <button className="button is-link">
-      Save changes
-    </button>
-  </p>
-  <p className="control">
-    <button className="button is-danger">
-      Delete post
-    </button>
-  </p>
+            <div className="field">
+                    <label for="" className="label">URL</label>
+                    <div className="control has-icons-left">
+                        <input type="text" placeholder="URL" className="input is-rounded" required/>
+                        <span className="icon is-small is-left">
+                        <i class="fas fa-cloud"></i>
+                        </span>
+                    </div>
+            </div>
+            <div class="field">
+  <label class="label">Subject</label>
+  <div class="control">
+    <div class="select">
+      <select>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </select>
+    </div>
+  </div>
 </div>
-            </div>
+<div className="field is-grouped">
+  <div className="control">
+    <button className="button is-info is-rounded">Submit</button>
+  </div>
+  <div className="control">
+    <button className="button is-link is-light is-rounded">Cancel</button>
+  </div>
+</div>
+
+</form>
    
 
             )
